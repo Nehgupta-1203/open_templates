886cd2cfa46c28b2320318a00a0ff561d6d833c1
Neha Gupta <8225994+Nehgupta-1203@users.noreply.github.com>
2025-07-07 23:51:49 +0000

    update app

--- orchestration_platform/app.py
+++ orchestration_platform/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from models import db, Deployment
