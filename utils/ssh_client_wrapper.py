29f83a6eacbb2a8f371d158c45082eaf2f488cf7
Nehgupta-1203 <8225994+Nehgupta-1203@users.noreply.github.com>
2025-07-16 23:46:09 +0100

    pkey updated

--- utils/ssh_client_wrapper.py
+++ utils/ssh_client_wrapper.py
@@ -5,7 +5,7 @@ def ssh_connection(ip,port,username,pem_file_path):
         ssh_client =paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         pkey = paramiko.RSAKey.from_private_key_file(pem_file_path)
-        ssh_client.connect(ip,port=port,username=username,password="")
+        ssh_client.connect(ip,port=port,username=username,pkey=pkey)
         return ssh_client
     except Exception as e:
         raise Exception(f"Failed to connect to {ip}:{port} - {str(e)}")
